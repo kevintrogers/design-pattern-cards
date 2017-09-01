ba64f94ea8706a1fc975cc8db99a077bc13467b4
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-01 17:49:26 +0000

    finished creational pattern outlines

--- design-cards/src/App.vue
+++ design-cards/src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class='card' >
+  <div class='card-list' >
     <div v-for="pattern in patterns">
-      <h3 class='card-title'> {{pattern.title}}</h3>
-      <div class='card-content'>    
-        <p> {{pattern.content}}</p>
+      <div class='card'>
+        <h3 class='card-title'> {{pattern.title}} Pattern</h3>
+        <div class='card-content'>    
+          <p> {{pattern.content}}</p>
+        </div>
+        <div class='card-content'>    
+          <p> {{pattern.useCase}}</p>
+        </div>
+        <div class='card-content'>
+          <a :href="pattern.source">Example</a>
+        </div>  
       </div>
-      <div class='card-content'>    
-        <p> {{pattern.useCase}}</p>
-      </div>
-     <div class='card-content'>
-    <a :href="pattern.source">Example</a>
-    </div>  
-    
-        
+  
     </div>
   </div>
 
@@ -30,7 +31,68 @@ export default {
           useCase: 'When this happens...',
           example: 'For Example...',
           source: 'http://spicymonkeymedia.com'
-        }
+        },
+        { title: 'Factory Method',
+          content: 'Creates objects without specifying exact class',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+        { title: 'Builder',
+          content: 'Groups object factories that have a common theme',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+        { title: 'Prototype',
+          content: 'Clones existing objects in order to create new objects',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+        { title: 'Dependency Injection',
+          content: 'A class accepts objects it requires from an injector instead of doing it directly',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+         { title: 'Lazy Initialization',
+          content: 'Delays creation of a resource consuming object until a more appropriate time',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+         { title: 'Multition',
+          content: 'Ensure a class only has named instances and provide a GPOA',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+         { title: 'Object Pool',
+          content: 'Recycles resources no longer in use in order to optimize resources',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+         { title: 'Singleton',
+          content: 'Ensure a class only has one instance and provide GPOA',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+         { title: 'Resource acquisition is initialization (RIIA)',
+          content: 'Ensure that resourcesare properly released by tying them to the lifespan of related object',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+         { title: 'Abstract Factory',
+          content: 'Groups object factories that have a common theme',
+          useCase: 'When this happens...',
+          example: 'For Example...',
+          source: 'http://spicymonkeymedia.com'
+        },
+        
       ]
     }
     
@@ -41,5 +103,14 @@ export default {
 </script>
 
 <style>
+.card-list {
+  background: pink;
+}
+.card {
+  margin-bottom: 2em;
+}
+.card-content{
+  border-bottom: 1px solid gray;
+}
 
 </style>
